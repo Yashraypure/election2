083440e624a6476a01a6712f633c7b64967307ff
Yashraypure <adiraypure66@gmail.com>
2023-06-10 03:01:07 +0530

    admin yashh

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2023 <a href="https://www.linkedin.com/in/yash-raypure-662b4b23b/">YASH RAYPURE</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2023 <a href="https://www.linkedin.com/in/yash-raypure-662b4b23b/">YASH RAYPURE</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
